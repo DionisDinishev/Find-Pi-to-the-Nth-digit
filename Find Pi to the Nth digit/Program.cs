0c0e5b102285d4e1597b83aa080a0d011d638b60
Dionis Dinishev <dionis.dinishev@intellaquest.com>
2021-02-03 23:12:46 +0100

    Add project files.

---
+++ Find Pi to the Nth digit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Pi_to_the_Nth_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
